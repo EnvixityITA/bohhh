eb73d6f686030d2fe302a309b972f162bb422b45
invv <koopigamezpotato@gmail.com>
2026-03-11 19:06:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+// semplice esempio: disegna un rettangolo
+ctx.fillStyle = 'red';
+ctx.fillRect(50, 50, 200, 100);
